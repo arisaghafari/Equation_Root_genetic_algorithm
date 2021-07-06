17942a88614c49dce4d8c4aa93e11360084f58ba
arisaghafari <arisa_ghafari@outlook.com>
2021-07-06 23:44:55 +0430

    Add: fitness method

--- Q2.py
+++ Q2.py
@@ -1,16 +1,45 @@
 import random
 import numpy as np
 
-class GLA(object):
+pop_len = 16
+
+class GA(object):
     def __init__(self, pop_size):
         self.pop_size = pop_size
+        self.pop_list = []
+        for _ in range(pop_size):
+            temp = ''
+            for _ in range(pop_len):
+                temp += str(random.randint(0, 1))
+            self.pop_list.append([temp, 0])
 
+        self.fitness_list = self.fitness()
+        #self.selection()
+        a = 2
     def fitness(self):
-        pass
+        fitness_list = []
+        for i in range(len(self.pop_list)):
+            decimal = bin_to_decimal(self.pop_list[i][0])
+            e = equation(decimal)
+            fitness_list.append(e)
+            self.pop_list[i][1] = e
+
+        return fitness_list
 
     def selection(self):  # using generation gap approach
-        pass
+        remove_num = int(self.pop_size * 0.3)
+        remove_list = []
+        fitness_temp = self.fitness_list
+        fitness_temp.sort()
+        for i in range(remove_num):
+            remove_list.append(fitness_temp[len(fitness_temp) - 1 - i])
 
+        count = 0
+        for j in range(len(self.fitness_list)):
+            if self.fitness_list[j] in remove_list:
+                del self.fitness_list[j - count]
+                del self.pop_list[j - count]
+                count += 1
     def cross_over(self):
         pass
 
@@ -21,6 +50,21 @@ class GLA(object):
 def equation(x):
     return 9 * x ** 5 - 194.7 * x ** 4 + 1680.1 * x ** 3 - 7227.94 * x ** 2 + 15501.2 * x - 13257.2
 
+def decimal_to_bin(decimal):
+    bin = decimal
+    return bin
+
+def bin_to_decimal(bin):
+    temp1 = 0
+    temp2 = 0
+    for i in range(int(pop_len / 2)):
+        temp1 += int(bin[i]) * 2 ** (int(pop_len / 2) - 1 - i)
+        temp2 += int(bin[i + int(pop_len / 2)]) * 2 ** (-(i + 1))
+    temp2 /= 10 ** int(pop_len / 2)
+    temp1 += temp2
+    return temp1
+
 if __name__ == "__main__":
-    pop_size = 40
-    gla = GLA(pop_size)
+    pop_size = 10
+    ga = GA(pop_size)
+    print(ga.pop_list)
